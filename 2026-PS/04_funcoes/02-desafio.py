b62fad757bb341c56d23f6d24c8795f0420c5b59
Luiz Otavio de Souza Freo <20251ctb0100003@estudantes.ifpr.edu.br>
2026-03-05 23:45:59 +0000

    Desafio aula 06 - Conceito B

--- 2026-PS/04_funcoes/02-desafio.py
+++ 2026-PS/04_funcoes/02-desafio.py
@@ -20,7 +20,6 @@ def exibir_cabecalho():
     print("=" * 40)
     print("  CALCULADORA DE NOTAS")
     print("=" * 40)
-    print()
 
 def exibir_rodape():
     """Mostra o rodapé do programa"""
@@ -28,6 +27,20 @@ def exibir_rodape():
     print("=" * 40)
     print("  SISTEMA ENCERRADO")
     print("=" * 40)
+
+def solicitar_notas(nome_aluno):
+    """Solicita e valida as notas do aluno"""
+    nota1 = float(input(f"Digite a primeira nota do(a) {nome_aluno} (0 - 10)): "))
+    while nota1 < 0 or nota1 > 10:
+        print("\nValor inválido! Digite uma nota entre 0 e 10")
+        nota1 = float(input(f"Digite a primeira nota do(a) {nome_aluno} (0 - 10)): "))
+    
+    nota2 = float(input(f"Digite a segunda nota do(a) {nome_aluno} (0 - 10): "))
+    while nota2 < 0 or nota2 > 10:
+        print("\nValor inválido! Digite uma nota entre 0 e 10")
+        nota2 = float(input(f"Digite a segunda nota do(a) {nome_aluno} (0 - 10)): "))
+    
+    return nota1, nota2
 
 def calcular_media(nota1, nota2):
     """Calcula a média de um aluno usando 2 notas"""
@@ -43,20 +56,26 @@ def verificar_situacao(media):
     else:
         return "Reprovado"
 
+def gerar_relatorio(nome, media, situacao):
+    """Gera um relatório formado, claro e organizado"""
+    print("-" * 40)
+    print(f"Nome    : {nome}")
+    print(f"Media   : {media:.2f}" )
+    print(f"Situação: {situacao}")
+    print("-" * 40)
+
 
 
 # ---- CÓDIGO PRINCIPAL ----
 exibir_cabecalho()
 
-nome  = input("Digite o nome do aluno: ")
-nota1 = float(input("Digite a primeira nota do aluno: "))
-nota2 = float(input("Digite a segunda nota do aluno: "))
-media = calcular_media(nota1, nota2)
-
-print()
-print(f"Nota 1  : {nota1}")
-print(f"Nota 2  : {nota2}")
-print(f"Media   : {media}" )
-print(f"Situação: {verificar_situacao(media)}")
+for i in range(1, 4):       # repete 3 vezes (1 para cada aluno)
+    print()
+    nome         = input(f"Digite o nome do aluno {i}: ")
+    nota1, nota2 = solicitar_notas(nome)
+    media        = calcular_media(nota1, nota2)
+    situacao     = verificar_situacao(media)
+    print()
+    gerar_relatorio(nome, media, situacao)
 
 exibir_rodape()
