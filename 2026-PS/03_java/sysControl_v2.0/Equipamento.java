b5ae1d4c7958221634abe7bf332b9f792bf856a8
Luiz Otavio de Souza Freo <20251ctb0100003@estudantes.ifpr.edu.br>
2026-07-29 12:21:49 +0000

    aula 30: Commit 2 - Atributos privados e método construtor

--- 2026-PS/03_java/sysControl_v2.0/Equipamento.java
+++ 2026-PS/03_java/sysControl_v2.0/Equipamento.java
@@ -1,3 +1,15 @@
 public class Equipamento {
-    
+    /* Atributos privados */
+    private String patrimonio;
+    private String descricao;
+    private int setor;
+    private boolean ativo;
+
+    /* Método construtor */
+    public Equipamento(String patrimonio, String descricao, int setor, boolean ativo) {
+        setPatrimonio(patrimonio);
+        setDescricao(descricao);
+        setSetor(setor);
+        setAtivo(ativo);
+    }
 }
--- 2026-PS/03_java/sysControl_v2.0/Main.java
+++ 2026-PS/03_java/sysControl_v2.0/Main.java
@@ -1,3 +1,7 @@
 public class Main {
-    
+    public static void Main(String[] args) {
+        Equipamento equipamento = new Equipamento("Luiz O.", "de S. F.", 25092009, true);
+
+        System.out.println("Testando...")
+    }
 }
