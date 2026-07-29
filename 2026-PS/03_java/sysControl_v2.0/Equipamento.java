ba9d878ec788cba8834254c26753838b94b9d005
Luiz Otavio de Souza Freo <20251ctb0100003@estudantes.ifpr.edu.br>
2026-07-29 12:37:31 +0000

    aula 30: Commit 3 - Getters, Setters e Validações

--- 2026-PS/03_java/sysControl_v2.0/Equipamento.java
+++ 2026-PS/03_java/sysControl_v2.0/Equipamento.java
@@ -1,4 +1,8 @@
+/* Equipamento.java */
+
+/* Classe Equipamento */
 public class Equipamento {
+
     /* Atributos privados */
     private String patrimonio;
     private String descricao;
@@ -11,5 +15,45 @@ public class Equipamento {
         setDescricao(descricao);
         setSetor(setor);
         setAtivo(ativo);
+    }
+
+    /* Getters */
+    public String getPatrimonio() {
+        return patrimonio;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getSetor() {
+        return setor;
+    }
+
+    public String getAtivo() {
+        return "Ativo" if ativo else "Inativo";
+    }
+
+    /* Setters e Validações */
+    public void setPatrimonio(String patrimonio) {
+        if (patrimonio != null && !patrimonio.isBlank()) {
+            this.patrimonio = patrimonio;
+        }
+    }
+
+    public void setDescricao(String descricao) {
+        if (descricao != null && !descricao.isBlank()) {
+            this.descricao = descricao;
+        }
+    }
+
+    public void setSetor(int setor) {
+        if (setor >= 0) {
+            this.setor = setor;
+        }
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
     }
 }
--- 2026-PS/03_java/sysControl_v2.0/Main.java
+++ 2026-PS/03_java/sysControl_v2.0/Main.java
@@ -1,3 +1,6 @@
+/* Main.java */
+
+/* Classe Main */
 public class Main {
     public static void Main(String[] args) {
         Equipamento equipamento = new Equipamento("Luiz O.", "de S. F.", 25092009, true);
